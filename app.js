032a853f5357e9a2b8d16fdb7da0aeb1ff7054ab
Dinu <coatu_dinu@yahoo.com>
2022-12-03 21:27:46 +0200

    Navigation login and sign up buttons functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import fakedb from "./data/fakedb.json" assert { type: "json" };
+import fakedb from "./data/fakedb.json" assert {type: "json"};
 
 console.log(fakedb); // fake database users
 
@@ -7,6 +7,24 @@ function swapForm() {
   document.querySelector(".registerW").classList.toggle("active");
 }
 
+function goToLogin() {
+  document.querySelector(".loginW").classList.add("active");
+  document.querySelector(".registerW").classList.remove("active");
+}
+
+function goToRegister() {
+  document.querySelector(".registerW").classList.add("active");
+  document.querySelector(".loginW").classList.remove("active");
+}
+
 document
   .querySelectorAll(".swap")
   .forEach((element) => element.addEventListener("click", swapForm));
+
+document.querySelector(".go_to_login").addEventListener("click", () => {
+  goToLogin();
+});
+
+document.querySelector(".go_to_register").addEventListener("click", () => {
+  goToRegister();
+});
