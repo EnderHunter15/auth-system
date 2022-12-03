d18e555843efb648e5df7c07ce03a45d0f42ab19
Dinu <coatu_dinu@yahoo.com>
2022-12-03 21:59:01 +0200

    Function to add a new user to the database, check if someone has the same username and return an error message.

--- app.js
+++ app.js
@@ -28,3 +28,31 @@ document.querySelector(".go_to_login").addEventListener("click", () => {
 document.querySelector(".go_to_register").addEventListener("click", () => {
   goToRegister();
 });
+
+document.querySelector(".login_button").addEventListener("click", login);
+
+function login() {
+  const email = document.getElementById("login_email").value;
+  const pass = document.getElementById("login_password").value;
+  const exists = fakedb.find((user) => user.username === email && user.password === pass);
+  if (exists) {
+    window.alert("OK");
+  } else {
+    window.alert("bad bad boi");
+  }
+}
+
+document.querySelector(".register_button").addEventListener("click", register);
+
+function register() {
+  const email = document.getElementById("register_email").value;
+  const pass = document.getElementById("register_password").value;
+  const exists = fakedb.find((item) => item.username === email && item.password === pass);
+  if (exists) {
+    window.alert("Email already taken");
+  } else {
+    fakedb.push({username: email, password: pass});
+    window.alert("Ochei");
+    console.log(fakedb);
+  }
+}
